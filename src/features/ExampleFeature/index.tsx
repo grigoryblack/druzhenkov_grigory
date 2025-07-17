824abd9ce7ab5ee5f5c00ff7e2e899c844f3f9f6
@gd <gd@callibri.ru>
2025-07-17 15:44:48 +0500

    init project

---
+++ src/features/ExampleFeature/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ExampleFeature = () => {
+  return (
+    <div>
+      <h1>Example Feature</h1>
+      <p>This is an example of FSD structure implementation</p>
+    </div>
+  );
+};
